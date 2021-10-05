a534a2886d60948268c25949ac2488df407bee8c
jny384 <66296986+jny384@users.noreply.github.com>
2021-10-06 03:20:26 +0900

    3rd : 1744

--- BJ_1744.py
+++ BJ_1744.py
@@ -1,3 +1,14 @@
+def match_number(L) : # 양수 음수 리스트 더해주는 과정이 같아서 함수로 묶음
+    sum = 0
+    if len(L) % 2 == 0: # 원소가 짝수개일 경우 두개씩 곱함
+        for i in range(0, len(L), 2):
+            sum += L[i] * L[i+1]
+    else: # 원소가 홀수개일 경우 두개씩 곱하고
+        for i in range(0, len(L)-1, 2):
+            sum += L[i] * L[i+1]
+        sum += L[len(L)-1] # 마지막 수는 더해줌
+    return sum
+
 list = [int(x) for x in input("숫자를 공백으로 구분하여 입력해주세요 : ").split()]
 positive = []
 negative = []
@@ -15,16 +26,6 @@ for i in range(len(list)) : # 0과 음수, 1, 양수 나누기
 positive.sort(reverse=True) # 양수는 내림차순
 negative.sort()
 
-def match_number(L) : # 양수 음수 리스트 더해주는 과정이 같아서 함수로 묶음
-    sum = 0
-    if len(L) % 2 == 0: # 원소가 짝수개일 경우 두개씩 곱함
-        for i in range(0, len(L), 2):
-            sum += L[i] * L[i+1]
-    else: # 원소가 홀수개일 경우 두개씩 곱하고
-        for i in range(0, len(L)-1, 2):
-            sum += L[i] * L[i+1]
-        sum += L[len(L)-1] # 마지막 수는 더해줌
-    return sum
 
 max_sum = match_number(positive) + match_number(negative) + one
 print(max_sum)
