31cba75a3904d5248b5e4ad2069e7c5278fe4bfe
jny384 <66296986+jny384@users.noreply.github.com>
2021-10-06 03:37:01 +0900

    3rd : 1744

--- BJ_1744.py
+++ BJ_1744.py
@@ -1,3 +1,22 @@
+
+import sys
+input = sys.stdin.readline
+
+list = int(input())
+# list = list(map(int, input().split()))
+positive = []
+negative = []
+one = 0
+
+for _ in range(list) : # 0과 음수, 1, 양수 나누기
+    l = int(input())
+    if l > 1 :
+        positive.append(l)
+    elif l == 1 : #1은 곱하지 않고 sum에 그대로 더 함
+        one += 1
+    else:
+        negative.append(l)
+
 def match_number(L) : # 양수 음수 리스트 더해주는 과정이 같아서 함수로 묶음
     sum = 0
     if len(L) % 2 == 0: # 원소가 짝수개일 경우 두개씩 곱함
@@ -9,23 +28,10 @@ def match_number(L) : # 양수 음수 리스트 더해주는 과정이 같아서
         sum += L[len(L)-1] # 마지막 수는 더해줌
     return sum
 
-list = [int(x) for x in input("숫자를 공백으로 구분하여 입력해주세요 : ").split()]
-positive = []
-negative = []
-one = 0
 
-for i in range(len(list)) : # 0과 음수, 1, 양수 나누기
-    if list[i] > 1 :
-        positive.append(list[i])
-    elif list[i] == 1 : #1은 곱하지 않고 sum에 그대로 더 함
-        one += 1
-    else:
-        negative.append(list[i])
 
 # 절대값이 큰 수부터 오게 정렬
 positive.sort(reverse=True) # 양수는 내림차순
 negative.sort()
-
-
 max_sum = match_number(positive) + match_number(negative) + one
 print(max_sum)
