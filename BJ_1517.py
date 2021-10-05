40562bd088aa8156c0242567bcd8d2dad95d0f9f
jny384 <66296986+jny384@users.noreply.github.com>
2021-10-06 03:46:16 +0900

    3rd : 1517

--- BJ_1517.py
+++ BJ_1517.py
@@ -1,29 +1,35 @@
-def merge_sort(start, end):
+import sys
+
+def mergeSort(start, end):
     global swap
     if start < end:
         mid = (start + end) // 2
-        merge_sort(start, mid)
-        merge_sort(mid + 1, end)
-        a, b = start, mid + 1
-        temp = []
+        mergeSort(start, mid)
+        mergeSort(mid + 1, end)
 
+        a = start
+        b = mid + 1
+        tmp = []
         while a <= mid and b <= end:
-            if N[a] <= N[b]:
-                temp.append(N[a])
+            if arr[a] <= arr[b]:
+                tmp.append(arr[a])
                 a += 1
+
             else:
-                temp.append(N[b])
+                tmp.append(arr[b])
                 b += 1
-                swap += (mid - a + 1)
+                swap += (mid - a + 1)  # 스와핑 했을 때 개수추가
         if a <= mid:
-            temp = temp + N[a:mid + 1]
-
+            tmp = tmp + arr[a:mid+1]
         if b <= end:
-            temp = temp + N[b:end + 1]
-        for i in range(len(temp)):
-            N[start+i] = temp[i]
+            tmp = tmp + arr[b:end+1]
 
-swap = 0
-N = [int(x) for x in input("숫자를 공백으로 구분하여 입력해주세요 : ").split()]
-merge_sort(0, len(N) - 1)
-print(swap)
+        for i in range(len(tmp)):
+            arr[start + i] = tmp[i]
+
+if __name__ == '__main__':
+    swap = 0
+    N = int(input())
+    arr = list(map(int, input().split()))
+    mergeSort(0, N - 1)
+    print(swap)
